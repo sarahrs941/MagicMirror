4de15d08659ea00376e9eb57a2cc6bca7ba635fd
sarahrs941 <48303366+sarahrs941@users.noreply.github.com>
2019-12-13 17:12:42 -0500

    Update runMirror.py

--- runMirror.py
+++ runMirror.py
@@ -2,36 +2,39 @@ import threading
 import subprocess
 import time
 import RPi.GPIO as GPIO
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.IN)
-GPIO.setup(13, GPIO.OUT)
+GPIO.setwarnings(False) 
+GPIO.setmode(GPIO.BOARD) # set board numbering
+GPIO.setup(11, GPIO.IN) # set PIR input pin
+GPIO.setup(13, GPIO.OUT) # set LED (on/off relay) output pin
 
 
 def startAlexa():
-    subprocess.Popen("sudo bash /home/pi/startsample.sh", shell=True)
+    subprocess.Popen("sudo bash /home/pi/startsample.sh", shell=True) 
 
 def startMirror():
     subprocess.Popen("pm2 start mm.sh", shell=True)
 
-def closeMirror():
-    subprocess.Popen("pm2 stop all", shell=True)
+#def closeMirror():
+    #subprocess.Popen("pm2 stop all", shell=True) results in pi loggin out
     
-close = 0
-startMirror()
-startAlexa()
-GPIO.output(13,GPIO.LOW)
+close = 1
+startMirror() # call function to start MagicMirror module
+startAlexa() # call function to start Alexa
+GPIO.output(13,GPIO.LOW) # set LED off
 
 while True:
     
-    i = GPIO.input(11)   
+    i = GPIO.input(11)   # get current motion
         
-    if (i == 1 and close == 1):	
-	GPIO.output(13,GPIO.LOW)          
+    if (i == 1): # while there is motion keep LED off
+	# GPIO.output(13,GPIO.HIGH) #  uncomment if using on/off relay
+	GPIO.output(13,GPIO.LOW)
+	time.sleep(0.5)
         
-    elif (i == 0):
+    elif (i == 0):  # while there is no motion keep LED on
 	print("I should shut off..")
+	# GPIO.output(13,GPIO.LOW) #  uncomment if using on/off relay
         GPIO.output(13,GPIO.HIGH)
-	close = 1
-	
+	time.sleep(0.5)
+
     
